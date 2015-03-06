ac1469e8e0b8503c3fbee6f44ba3114cbcb79878
nikolakn <nkcodeplus@gmail.com>
2015-03-06 10:35:34 +0100

    bin

--- TableItem.h
+++ TableItem.h
@@ -1,3 +1,13 @@
+/*
+* (C) Copyright 2015
+* Author: Nikola Knezevic <nkcodeplus@gmail.com>
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; version 2
+* of the License.
+*/
+
 #ifndef TABLEITEM_H
 #define TABLEITEM_H
 #include <QTableWidgetItem>
